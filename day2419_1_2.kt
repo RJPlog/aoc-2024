027ed7cc5e1aa47345e469285b6a6b903db1385d
RJPlog <tfrenz67@gmail.com>
2024-12-19 11:02:15 +0100

    Update day2419_1_2.kt
    part1 works for given example

--- day2419_1_2.kt
+++ day2419_1_2.kt
@@ -1,5 +1,34 @@
 import java.io.File
 
+fun LinLay(pattern: String, layout: String, count: Int = 0): Int {
+    
+    var insert = " ".repeat(count)
+
+    var layPossible = 0
+
+    pattern.split(", ").forEach {
+        var currPat = it
+        //println("$insert run with $currPat, $layout,  ${currPat.length}, ${layout.length}")
+        if (currPat.length >= layout.length) {
+            if (currPat == layout) {
+                layPossible += 1
+            } 
+        } else {
+            //println("$insert    else : ${layout.take(currPat.length)} vs $currPat")
+            if (layout.take(currPat.length) == currPat) {
+              //  println( "$insert     next level: ${layout.drop(currPat.length)}")
+                layPossible += LinLay(pattern, layout.drop(currPat.length), count+4)
+            } else {
+                layPossible += 0
+            }
+        }
+    }
+
+
+
+    return layPossible
+}
+
 fun main() {
     var t1 = System.currentTimeMillis()
 
@@ -12,21 +41,22 @@ fun main() {
         } else if (i > 1) {
             patterns2Create.add(it)
         }
-        i += 0
+        i += 1
     }
-
-    println(availablePatterns)
-    println(patterns2Create)
 
     println("--- Day 19: Linen Layout ---")
     
+    var solution1 = 0 
     
+    patterns2Create.forEach {
+        println("$it -> ${LinLay(availablePatterns, it)}")
+        if (LinLay(availablePatterns, it, 0) >= 1) solution1 += 1
+    }
 
-    println("  part1: designs are possible") 
-    
+    println("  part1: $solution1 designs are possible") 
     
     //println("  part2:  i")
     
     t1 = System.currentTimeMillis() - t1
-	println("puzzle solved in ${t1} ms")
+    println("puzzle solved in ${t1} ms")
 }
