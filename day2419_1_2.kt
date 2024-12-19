f663f285f450f1d893cbb019325aab31612d8a92
RJPlog <tfrenz67@gmail.com>
2024-12-19 09:14:49 +0100

    Update day2419_1_2.kt
    initial setup

--- day2419_1_2.kt
+++ day2419_1_2.kt
@@ -1,7 +1,24 @@
+import java.io.File
+
 fun main() {
     var t1 = System.currentTimeMillis()
 
-    println("--- Day 19: Linen Layout ---)
+    var i = 0
+    var availablePatterns = ""
+    var patterns2Create = mutableListOf<String>()
+    File("day2419_puzzle_input.txt").forEachLine {
+        if (i == 0) {
+            availablePatterns = it
+        } else if (i > 1) {
+            patterns2Create.add(it)
+        }
+        i += 0
+    }
+
+    println(availablePatterns)
+    println(patterns2Create)
+
+    println("--- Day 19: Linen Layout ---")
     
     
 
