6b26561ee42a8e72655e7f0e161f72d8222ca4ae
RJPlog <tfrenz67@gmail.com>
2024-12-19 09:03:01 +0100

    Create day2419_1_2.kt

---
+++ day2419_1_2.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var t1 = System.currentTimeMillis()
+
+    println("--- Day 19: Linen Layout ---)
+    
+    
+
+    println("  part1: designs are possible") 
+    
+    
+    //println("  part2:  i")
+    
+    t1 = System.currentTimeMillis() - t1
+	println("puzzle solved in ${t1} ms")
+}
