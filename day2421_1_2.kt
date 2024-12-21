866d14886d3999f8975bc37d88ca2cdd085b2096
RJPlog <tfrenz67@gmail.com>
2024-12-21 17:06:57 +0100

    Update day2421_1_2.kt
    addaption to catch all possible variants

--- day2421_1_2.kt
+++ day2421_1_2.kt
@@ -1,27 +1,4 @@
-fun keypadCon(pins: List<String>): Int {
-    
-    var result = 0
-    
-    var n2d = mutableMapOf<String,List<String>>()
-    n2d.put("AA", listOf("A"))
-    n2d.put("A0", listOf("<A"))
-    n2d.put("02", listOf("^A"))
-    n2d.put("29", listOf("^^>A"))  // !
-    n2d.put("9A", listOf("vvvA"))
-    n2d.put("A9", listOf("^^^A"))
-    n2d.put("98", listOf("<A"))
-    n2d.put("80", listOf("vvvA"))
-    n2d.put("0A", listOf(">A"))
-    n2d.put("A1", listOf("^<<A"))  // !
-    n2d.put("17", listOf("^^A"))
-    n2d.put("79", listOf(">>A"))
-    n2d.put("A4", listOf("^^<<A"))  // !
-    n2d.put("45", listOf(">A"))
-    n2d.put("56", listOf(">A"))
-    n2d.put("6A", listOf("vvA"))  
-    n2d.put("A3", listOf("^A"))
-    n2d.put("37", listOf("^^<<A", "<<^^A", "<^<^", "^<^<^"))  // there is a differenz between ^^<< and <<^^  -> follow that up. There is even a strange thing that <<^^ makes 57 as a min
-    
+fun transN2N(pin: String): List<String> {
     var n2n = mutableMapOf<String,String>()
     n2n.put("AA", "A")
     n2n.put("A<", "v<<A")
@@ -44,13 +21,59 @@ fun keypadCon(pins: List<String>): Int {
     n2n.put("<^", ">^A")
 
     
-    pins.forEach {
-        var startPin = "A"
-        var pin = it
-        var firstSeq = mutableListOf<String>()
-        firstSeq.add("")
-        println("pin: $pin")
-                pin.forEach {
+    var startPin = "A"
+    var firstSeq = mutableListOf<String>()
+    firstSeq.add("")
+  //  println("pin: $pin")
+    pin.forEach {
+                    var firstSeqNew = mutableListOf<String>()
+                    var move = startPin + it.toString()
+                    if (n2n.containsKey(move)) {
+                        n2n.getValue(move).forEach {
+                            for (j in 0..firstSeq.size-1) {
+                                firstSeqNew.add(firstSeq[j] + it)
+                            }
+                        }
+                        
+                    } else {
+                        println("   $move missing in n2d map")
+                    }
+                    firstSeq.clear()
+                    firstSeq.addAll(firstSeqNew)
+                    startPin = it.toString()
+                }
+    //    println("next Seq: $firstSeq, startPin: $startPin")
+        
+    return firstSeq
+}
+
+
+fun transN2D(pin: String): List<String> {
+    var n2d = mutableMapOf<String,List<String>>()
+    n2d.put("AA", listOf("A"))
+    n2d.put("A0", listOf("<A"))
+    n2d.put("02", listOf("^A"))
+    n2d.put("29", listOf("^^>A"))  // !
+    n2d.put("9A", listOf("vvvA"))
+    n2d.put("A9", listOf("^^^A"))
+    n2d.put("98", listOf("<A"))
+    n2d.put("80", listOf("vvvA"))
+    n2d.put("0A", listOf(">A"))
+    n2d.put("A1", listOf("^<<A"))  // !
+    n2d.put("17", listOf("^^A"))
+    n2d.put("79", listOf(">>A"))
+    n2d.put("A4", listOf("^^<<A"))  // !
+    n2d.put("45", listOf(">A"))
+    n2d.put("56", listOf(">A"))
+    n2d.put("6A", listOf("vvA"))  
+    n2d.put("A3", listOf("^A"))
+    n2d.put("37", listOf("^^<<A", "<<^^A", "<^<^", "^<^<^"))  // there is a differenz between ^^<< and <<^^  -> follow that up. There is even a strange thing that <<^^ makes 57 as a min
+    
+    var startPin = "A"
+    var firstSeq = mutableListOf<String>()
+    firstSeq.add("")
+//    println("pin: $pin")
+    pin.forEach {
                     var firstSeqNew = mutableListOf<String>()
                     var move = startPin + it.toString()
                     if (n2d.containsKey(move)) {
@@ -67,30 +90,39 @@ fun keypadCon(pins: List<String>): Int {
                     firstSeq.addAll(firstSeqNew)
                     startPin = it.toString()
                 }
-        println("firstSeq: $firstSeq, startPin: $startPin")
+       // println("firstSeq: $firstSeq, startPin: $startPin")
         
-        /*
-        for (i in 0..1) {
-            var nextSeq = ""
-            // repeat with num2num pads
-            
-                firstSeq.forEach {
-                    var move = startPin + it.toString()
-                    if (n2n.containsKey(move)) {
-                     	nextSeq += n2n.getValue(move)
-                        //println("move: $move, $nextSeq")
-                    } else {
-                        println("   $move missing in n2n map")
-                    }
-                    startPin = it.toString()
-                }
-            firstSeq = nextSeq
-            println("$i: i-te transformation: $firstSeq")
+    return firstSeq
+}
+
+fun keypadCon(pins: List<String>): Int {
+    
+    var result = 0
+    
+
+
+    var possibleSequencesFirstTrans = mutableListOf<String>()
+        var possibleSequencesSecondTrans = mutableListOf<String>()
+            var possibleSequencesThirdTrans = mutableListOf<String>()
+    
+    pins.forEach {
+        println(it)
+        possibleSequencesFirstTrans.addAll(transN2D(it))
+        
+        possibleSequencesFirstTrans.forEach{
+            possibleSequencesSecondTrans.addAll(transN2N(it))
         }
         
-        println("pin $pin: -> ${firstSeq.length}, pinvalue: ${pin.filter {it.isDigit()}.toInt()}")
-        result += firstSeq.length * pin.filter {it.isDigit()}.toInt()
-        */
+        //possibleSequencesSecondTrans.forEach {
+        //    possibleSequencesThirdTrans.addAll(transN2N(it))
+        //}
+        
+                println(possibleSequencesFirstTrans)
+            //    println(possibleSequencesSecondTrans)
+          //      println(possibleSequencesThirdTrans)
+        //println("pin $pin: -> ${firstSeq.length}, pinvalue: ${pin.filter {it.isDigit()}.toInt()}")
+        //result += firstSeq.length * pin.filter {it.isDigit()}.toInt()
+
     }
     
     return result
