24fd527e56b070015345763466c4cc2f1e45796e
RJPlog <tfrenz67@gmail.com>
2024-12-24 23:53:06 +0100

    Update day2424_1_2.kt
    finally also part2 of day24 solved

--- day2424_1_2.kt
+++ day2424_1_2.kt
@@ -1,31 +1,94 @@
 import java.io.File
 
-fun main() {
+fun crossedWires(gates: List<String>): String {
+	// sum = (xi XOR yi) XOR Ci-1
+	// Ci = (xi AND yi) OR (ci-1 AND (xi XOR yi)
 
+	var wrongCon = mutableListOf<String>()
+
+	var sigAList = mutableListOf<String>()
+	var sigBList = mutableListOf<String>()
+
+	// fill sigA and Sig B list
+	for (i in 1..44) {
+		var x = "x" + i.toString().padStart(2, '0')
+		var y = "y" + i.toString().padStart(2, '0')
+
+		var out: String
+		gates.forEach {
+			if (it.contains(x + " XOR " + y) || it.contains(y + " XOR " + x)) {
+				out = it.substringAfter("-> ")
+				if (!sigAList.contains(out)) sigAList.add(out)
+			}
+			if (it.contains(x + " AND " + y) || it.contains(y + " AND " + x)) {
+				out = it.substringAfter("-> ")
+				if (!sigBList.contains(out)) sigBList.add(out)
+			}
+		}
+	}
+
+	for (i in 1..44) {
+		var x = "x" + i.toString().padStart(2, '0')
+		var y = "y" + i.toString().padStart(2, '0')
+
+		var out: String
+
+		gates.forEach {
+			// if xn xor yn are input, output is not able to be zn (at least for all after lsb
+			if (it.contains(x + " XOR " + y) || it.contains(y + " XOR " + x)) {
+				out = it.substringAfter("-> ")
+				if (out.contains("z")) {
+					if (!wrongCon.contains(out)) wrongCon.add(out)
+				}
+			}
+			// zn can only be output of an XOR element (not valid for last element!
+			if (it.contains(" AND ") || it.contains(" OR ")) {
+				out = it.substringAfter("-> ")
+				if (out.contains("z") && out != "z45") {
+					if (!wrongCon.contains(out)) wrongCon.add(out)
+				}
+			}
+			// output of an XOR with no input xn or yn is not allowed to be other than zn -->> hier weitermachen.
+			if (!it.contains("x") && it.contains("XOR")) {
+				out = it.substringAfter("-> ")
+				if (!out.contains("z")) {
+					if (!wrongCon.contains(out)) wrongCon.add(out)
+				}
+			}
+			// any input of XOR is not allowed to be SigB
+			if (it.contains("XOR") && it.substringBefore("->").contains(sigBList[i - 1])) {
+				if (!wrongCon.contains(sigBList[i - 1])) wrongCon.add(sigBList[i - 1])
+			}
+			// any input of OR is not allowed to be SigA
+			if (it.contains(" OR") && it.substringBefore("->").contains(sigAList[i - 1])) {
+				if (!wrongCon.contains(sigAList[i - 1])) wrongCon.add(sigAList[i - 1])
+			}
+		}
+	}
+	return wrongCon.sorted().joinToString(",")
+}
+
+fun main() {
 	var t1 = System.currentTimeMillis()
 
 	println("--- Day 24: Crossed Wires ---")
 
-	var puzzleInput = mutableListOf<String>()
-
 	var lines = mutableListOf<String>()
 	var gates = mutableListOf<String>()
-	
+
 	File("day2424_puzzle_input.txt").forEachLine {
 		if (it.contains("->")) {
-			 gates.add(it)
+			gates.add(it)
 		} else if (it.contains(":")) {
 			lines.add(it)
 		}
 	}
-	
-	
 
-	println(lines)
-	println(gates)
-	
+	// calc solution for part two before modifing input for part one
+	var solution2 = crossedWires(gates)
+
+	// calc part one 			
 	while (!gates.isEmpty()) {
-		println()
 		var network = gates.joinToString(",")
 		lines.forEach {
 			network = network.replace(it.split(": ")[0], it.split(": ")[1])
@@ -44,27 +107,23 @@ fun main() {
 		network = network.replace("0 XOR 0", "0")
 
 		gates.clear()
-	
+
 		network.split(",").forEach {
 			if (it.substringBefore(" ->") == "0") {
 				lines.add(it.split(" -> ")[1] + ": " + "0")
-			}  else if (it.substringBefore(" ->") == "1") {
+			} else if (it.substringBefore(" ->") == "1") {
 				lines.add(it.split(" -> ")[1] + ": " + "1")
-			}else {
+			} else {
 				gates.add(it)
 			}
 		}
-		
-		println(network)
-		println(lines)
-		println(gates)
 	}
-		
-	var solution1 = lines.filter {it[0] == 'z'}.sortedDescending().map {it.takeLast(1)}.joinToString("").toLong(2)
+
+	var solution1 = lines.filter { it[0] == 'z' }.sortedDescending().map { it.takeLast(1) }.joinToString("").toLong(2)
+
 	println("  part1: the system output is $solution1")
 
-//	var solution2 = lanParty2(puzzleInput)
-//	println("   part2: the system output is $solution2")
+	println("  part2: these are the wrong connected wires: $solution2")
 
 	t1 = System.currentTimeMillis() - t1
 	println("puzzle solved in ${t1} ms")
