25fba860eb0e6ba023baa7052fc4612bd4621c4a
RJPlog <tfrenz67@gmail.com>
2024-12-27 10:19:58 +0100

    Update day2417_1_2.kt
    finalliy solved part2

--- day2417_1_2.kt
+++ day2417_1_2.kt
@@ -1,17 +1,13 @@
-fun compute(code: List<Long>, A: Long, B: Long, C: Long): String {
-    var output = mutableListOf<Long>()
+fun compute(code: List<Long>, A: Long, B: Long, C: Long, part: Int = 0): String {
     
+	var output = mutableListOf<Long>()
     var A = A
     var B = B
     var C = C
-    
-    var inst = 0
-    
-    //println("INI A: $A, B: $B, C: $C, inst: $inst")
-    
+    var inst = 0    
     var i = 0L
+	
     while (inst < code.size) {
- 
         var opcode = code[inst]
         var operand = code[inst+1]
         var comboOp = when (operand) {
@@ -24,7 +20,6 @@ fun compute(code: List<Long>, A: Long, B: Long, C: Long): String {
             6L -> C
             else -> -1L
         }
-        //println("$i: do $opcode with op $operand or combo $comboOp")
         when (opcode) {
             0L -> { //adv
                 var divisor = 1
@@ -55,7 +50,7 @@ fun compute(code: List<Long>, A: Long, B: Long, C: Long): String {
             }
             5L -> { //out
                 output.add(comboOp % 8)
-                if (code.take(output.size) != output) return "-1L"
+                //if (code.take(output.size) != output) return "-1L"
                 inst += 2
             }
             6L -> { // bdv
@@ -75,10 +70,8 @@ fun compute(code: List<Long>, A: Long, B: Long, C: Long): String {
                 inst += 2  
             }
         }
-        //println("    A: $A, B: $B, C: $C, inst: $inst")
     i += 1  
-    }
-    
+    }   
     return output.joinToString(",")
 }
 
@@ -88,11 +81,11 @@ fun main() {
 
     println("--- Day 17: Chronospatial Computer ---")
     
-    var A = 2024L
+    var A = 52042868L
     var B = 0L
     var C = 0L
     
-    var puzzleInput = listOf(0,3,5,4,3,0)
+    var puzzleInput = listOf(2,4,1,7,7,5,0,3,4,4,1,7,5,5,3,0)
 
     var code = mutableListOf<Long>()
     puzzleInput.forEach {
@@ -100,19 +93,29 @@ fun main() {
     }
     
     
-    var solution1 = compute(code, A,B,C)
-
-    println("  part1: you get $solution1") 
+    var solution1 = compute(code, A, B, C, 1)
     
-    var i = 0L
-    val codeString = code.joinToString(",")
-
-    while (compute(code, i, 0, 0) != codeString) {
-        //if (i % 1000L == 0L) println(i)
-        i += 1
-    }
-
-    println("  part2:  the lowest positive initial value that causes the program to output a copy of itself is: $i")
+	println() 
+    println("  part1: you get $solution1") 
+    	
+	
+	//println(" iterations to determine A for part2:")
+	var solution2 = 0L
+	
+	for (j in 1..puzzleInput.size) {
+		var partString = puzzleInput.takeLast(j).joinToString(",")
+		//println("  partString: $partString")
+			for (i in 0..1000000) {
+				if (compute(code, solution2 + i.toLong(), 0,0,1) == partString) {
+					//println("    match:  $i check: ${compute2(code, solution2 + i.toLong(), 0,0,1)}")
+					//println("    at: ${solution2 + i.toLong()}")
+					solution2 = (solution2 + i) * 8
+					break
+				}	
+			}		
+	}
+	
+    println("  part2:  the lowest positive initial value that causes the program to output a copy of itself is: ${solution2/8}") // to low 267265166221856
     
     t1 = System.currentTimeMillis() - t1
 	println("puzzle solved in ${t1} ms")
