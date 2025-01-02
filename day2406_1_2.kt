674debae43cfb5dcb5eee22b90cdc93a46026e07
RJPlog <tfrenz67@gmail.com>
2025-01-02 16:52:33 +0100

    Update day2406_1_2.kt

--- day2406_1_2.kt
+++ day2406_1_2.kt
@@ -193,4 +193,194 @@ fun main() {
     //                        b) an jeder Stelle an der er vorbei kommt zum nächsten Schritt ein Hindernis stellen
     // bei b) hab ich ursprünglich nur die Anzahl der Stellen genommen (ergebnis aus Teil 1), nicht die wirkliche Zahl der Stellen, aber auch das hat nichts geholfen.
     // manche Stellen sind doppelt im Zählen, aber auch das hiflt nichts.
+
+
+    //method b)
+    /* 
+            var result2 = 0
+            for (y in 0..9) {  // 102 -  -9
+				for (x in 0..9) {  // 102 - 9    
+					if (!(x == 4 && y == 6)) {   //  47 / 49       4 -6
+						var steps = guard(2,x,y)
+						if (steps == -1) {
+                            result2 += 1
+						    println("x: $x, y: $y, index: ${x+10*y} steps: $steps, result = $result2")
+                        }
+					}
+				}
+			}
+	
+            println("   ${result2} different positions could be choosen for an obstruction") //5822/5821/5304 to high 1288/1287 / 
+
+    */
+}
+
+
+fun guard(in1: Int, xMod: Int, yMod: Int ): Int {
+
+
+    var yardMap = ""
+
+    var xPos = 0
+
+    var yPos = 0
+
+    var width = 0
+
+    var height = 0
+
+    var i = 0
+
+    var direction = "^"
+
+    File("day2406_puzzle_input.txt").forEachLine {
+        width = it.length
+        yardMap += it
+        if (it.contains("^")) {
+            xPos = it.indexOf("^")
+            yPos = i
+        }
+        i += 1
+    }
+    height = i   
+
+//   println("xPos;: $xPos, yPos: $yPos")
+//	println("width: $width, height: $height")
+
+	if (xMod >= 0 && yMod >= 0) {
+	yardMap = yardMap.replaceRange(xMod + yMod*width, xMod + yMod*width + 1, "#")
+	}
+
+    var guardInYard = true
+    var guardInLoop = false
+    var lastFourTurns = mutableListOf<Int>()
+
+   
+
+    i = 0
+
+    while (guardInYard && !guardInLoop) {
+
+		
+		// move guard and turn if necessary
+        if (direction == "^") {
+
+            if (yPos - 1 >= 0) {
+
+                if (yardMap[xPos + width*(yPos-1)] != '#') {
+                    //yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + yPos*width + 1, "^")
+                    yPos -= 1
+					if (yardMap[xPos + yPos*width].toString() == direction) {
+						//guardInLoop = true
+					}
+                    yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + yPos*width + 1,  direction)
+                } else {
+                    direction = ">"
+					lastFourTurns.add(xPos + yPos*width)
+				    if (yardMap[xPos + yPos*width].toString() == direction) {
+						//guardInLoop = true
+					}
+                    yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + yPos*width + 1,  direction)
+                }
+            } else {
+                guardInYard = false
+            }
+
+        } else if (direction == ">") {
+            if (xPos + 1 < width) {
+                if (yardMap[xPos + 1 + width*(yPos)] != '#') {
+                    //yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + 1 + yPos*width, ">")
+                    xPos += 1
+					if (yardMap[xPos + yPos*width].toString() == direction) {
+						//guardInLoop = true
+					}					
+                    yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + 1 + yPos*width,  direction)
+                } else {
+                    direction = "v"
+					lastFourTurns.add(xPos + yPos*width)
+                    if (yardMap[xPos + yPos*width].toString() == direction) {
+						//guardInLoop = true
+					}
+                    yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + 1 + yPos*width,  direction)
+                }
+            } else {
+                guardInYard = false
+            }
+        } else if (direction == "v") {
+            if (yPos + 1 < height) {
+                if (yardMap[xPos + width*(yPos+1)] != '#') {
+                    //yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + yPos*width + 1, "v")
+                    yPos += 1
+					if (yardMap[xPos + yPos*width].toString() == direction) {
+					//	guardInLoop = true
+					}
+                    yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + yPos*width + 1,  direction)
+                } else {
+                    direction = "<"
+					lastFourTurns.add(xPos + yPos*width)
+					if (yardMap[xPos + yPos*width].toString() == direction) {
+						//guardInLoop = true
+					}
+                    yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + yPos*width + 1,  direction)
+                }
+            } else {
+                guardInYard = false
+            }
+
+        } else if (direction == "<") {
+
+            if (xPos - 1 >= 0) {
+				 if (yardMap[xPos - 1 + width*(yPos)] != '#') {
+                    //yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + yPos*width + 1, "<")
+                    xPos -= 1
+					if (yardMap[xPos + yPos*width].toString() == direction) {
+						//guardInLoop = true
+					}
+                    yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + yPos*width + 1,  direction)
+                } else {
+                    direction = "^"
+					 lastFourTurns.add(xPos + yPos*width)
+					if (yardMap[xPos + yPos*width].toString() == direction) {
+						//guardInLoop = true
+					}
+                    yardMap = yardMap.replaceRange(xPos + yPos*width, xPos + yPos*width + 1,  direction)
+                }
+            } else {
+                guardInYard = false
+            }
+        }
+
+         i += 1
+		
+		if (i > width * height) guardInLoop = true
+		if (lastFourTurns.size > 8 && lastFourTurns.takeLast(4) == lastFourTurns.takeLast(8).dropLast(4)) guardInLoop= true
+        if (guardInLoop) println("----------Loop-------------------")
+
+         if (false) {
+			 println("x $xPos, y $yPos")
+            for (y in 0..height-1) {
+                for (x in 0..width-1) {
+                    print(yardMap[x + y * width])
+                }
+                println()
+            }
+        println()
+        } 
+
+    }
+
+    if (in1 == 1) {
+    	
+        return yardMap.count { it != '.' && it != '#' }
+
+    } else {
+
+		if (guardInLoop) {
+			return -1
+		} else {
+			return i
+		}
+
+    }
+
 }
