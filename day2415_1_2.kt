c8f2cfbf189ba15afa819e6bc781853084944282
RJPlog <tfrenz67@gmail.com>
2025-01-03 11:26:45 +0100

    Update day2415_1_2.kt
    move left for part 2

--- day2415_1_2.kt
+++ day2415_1_2.kt
@@ -16,76 +16,62 @@ fun warehouse2(puzzleInput1: String, puzzleInput2:String, w: Int, h: Int, part: 
     var w = 2*w
 
     // new pushing algorithm
-    /*
+
+        // for development purpose
+        println("initial state")
+        warehouse.chunked(w).forEach{
+                println(it)
+            }
+            println()
+    
     puzzleInput2.forEach {
         var x = warehouse.indexOf('@') % w
         var y = warehouse.indexOf('@') / w
         
         when (it) {     			
             '^' -> {
-                if (warehouse[(x)+ w*(y-1)] == '.') {
-                    warehouse = warehouse.replaceRange((x)+ w*(y), (x)+ w*(y)+1, ".")
-                    warehouse = warehouse.replaceRange((x)+ w*(y-1), (x)+ w*(y-1)+1, "@")
-                } else if (warehouse[(x)+ w*(y-1)] == 'O') {
-                    val pR = "\\.(.{" + (w-1).toString() + "}O)+.{" + (w-1).toString() + "}@"
-    				val pD = pR.toRegex()  //"""\.(.{7}O)+.{7}@""".toRegex()
-                    val matchD = pD.find(warehouse)
-                    if (matchD != null) {
-                    	warehouse = warehouse.replaceRange(x + w*y, x + w*y + 1, ".")
-                        warehouse = warehouse.replaceRange(x + w*(y-1), x + w*(y-1) + 1, "@")
-                        warehouse = warehouse.replaceRange(x + w*y - (matchD!!.value.length-1), x + w*y - (matchD!!.value.length-1) + 1, "O")
-                    }
-                }
-                //println("up")
+                println("up")
             }
             '>' -> {
-                if (warehouse[(x+1)+ w*(y)] == '.') {
-                    warehouse = warehouse.replaceRange((x)+ w*(y), (x)+ w*(y)+1, ".")
-                    warehouse = warehouse.replaceRange((x+1)+ w*(y), (x+1)+ w*(y)+1, "@")
-                } else if (warehouse[(x+1)+ w*(y)] == 'O') {
-                    val pR = """@O+\.""".toRegex()
-                    val match = pR.find(warehouse)
-                    if (match != null) {
-                    	warehouse = warehouse.replace(match!!.value, "." + match!!.value.dropLast(1))
-                    }
-                }
-                //println("right")
-                }
+                println("right")
+            }
             'v' -> {
-                if (warehouse[(x)+ w*(y+1)] == '.') {
-                    warehouse = warehouse.replaceRange((x)+ w*(y), (x)+ w*(y)+1, ".")
-                    warehouse = warehouse.replaceRange((x)+ w*(y+1), (x)+ w*(y+1)+1, "@")
-                } else if (warehouse[(x)+ w*(y+1)] == 'O') {
-                    val pR = "@(.{" + (w-1).toString() + "}O)+.{" + (w-1).toString() + "}\\."
-    				val pD = pR.toRegex()  //"""@(.{7}O)+.{7}\.""".toRegex()
-                    val matchD = pD.find(warehouse)
-                    if (matchD != null) {
-                    	warehouse = warehouse.replaceRange(x + w*y, x + w*y + 1, ".")
-                        warehouse = warehouse.replaceRange(x + w*(y+1), x + w*(y+1) + 1, "@")
-                        warehouse = warehouse.replaceRange(x + w*y + matchD!!.value.length-1, x + w*y + matchD!!.value.length-1 + 1, "O")
-                    }
-                }
-                //println("down")
-                }
+                println("down")
+            }
             '<' -> {
-                //println("left")
-                if (warehouse[(x-1)+ w*(y)] == '.') {
-                    warehouse = warehouse.replaceRange((x)+ w*(y), (x)+ w*(y)+1, ".")
-                    warehouse = warehouse.replaceRange((x-1)+ w*(y), (x-1)+ w*(y)+1, "@")
-                } else if (warehouse[(x-1)+ w*(y)] == 'O') {
-                    val pR = """\.O+@""".toRegex()
-                    val match = pR.find(warehouse)
-                    if (match != null) {
-                    	warehouse = warehouse.replace(match!!.value, match!!.value.drop(1) + ".")
+                println("left")
+                var moveArea = "@"
+                var moveIndex = 1
+                var moveEval = false
+                var movePos = false
+                while (!moveEval){
+                    if (warehouse[(x-moveIndex)+w*y] == ']') {
+                        moveArea = "[]" + moveArea
+                        moveIndex += 2
+                    } else if (warehouse[(x-moveIndex)+w*y] == '.') {
+                        moveEval = true
+                        movePos = true
+                    } else if (warehouse[(x-moveIndex)+w*y] == '#') {
+                        moveEval = true
                     }
+                }
+                if (movePos) {
+                    warehouse = warehouse.replaceRange((x-moveIndex)+w*y, (x-moveIndex)+w*y + moveArea.length, moveArea)
+                    warehouse = warehouse.replaceRange(x + w*y, x + w*y + 1, ".")
                 }
             }
         } 
+            // for development purpose
+        warehouse.chunked(w).forEach{
+            println(it)
+        }
+        println()
 
     }
-    */
+
 
     // for development purpose
+    println("final state")
     warehouse.chunked(w).forEach{
             println(it)
         }
